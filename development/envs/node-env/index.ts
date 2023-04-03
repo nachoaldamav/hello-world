23cc71275459f26b39ef8c4300ddcddc47afe61a
ashanfernando <ashan256@gmail.com>
2023-04-03 11:34:52 +0530

    Adding React and Node Environments

---
+++ development/envs/node-env/index.ts
@@ -0,0 +1,5 @@
+import { NodeEnvAspect } from './node-env.aspect';
+
+export type { NodeEnvMain } from './node-env.main.runtime';
+export default NodeEnvAspect;
+export { NodeEnvAspect };
---
+++ development/envs/react-env/index.ts
@@ -0,0 +1,3 @@
+import { ReactEnv } from './react-env.bit-env';
+export { ReactEnv };
+export default ReactEnv;
