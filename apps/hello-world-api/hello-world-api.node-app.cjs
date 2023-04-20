7755ff347ca1fe2bd57af1e4edc20be772591e4e
ashanfernando <ashan256@gmail.com>
2023-04-20 17:46:32 +0530

    Updating the node deploy

--- apps/hello-world-api/hello-world-api.node-app.cjs
+++ apps/hello-world-api/hello-world-api.node-app.cjs
@@ -1,6 +1,18 @@
 /** @type {import("@teambit/node").NodeAppOptions} */
+
+import { Ssh } from '@greetings/development.deployers.ssh-deployer';
+
+const config = {
+  host: 'xxxx',
+  username: 'xxx',
+  cwd: './',
+  privateKey: '/Users/<my_user>/key.pem',
+  runCommand: 'npm install && npm run build --if-present && forever stopall && nohup forever api/index.js  &> /dev/null &',
+};
+
 module.exports.default = {
   name: 'hello-world-api',
   entry: require.resolve('./hello-world-api.app-root'),
+  deploy: Ssh.deploy(config),
   portRange: [3000, 4000],
 };
