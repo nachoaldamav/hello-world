81b2e1dfe8901df6e60b2b7482bf979087be5695
ashanfernando <ashan256@gmail.com>
2023-04-20 15:20:19 +0530

    Updating the deployer

--- apps/hello-world-api/hello-world-api.deployer.ts
+++ apps/hello-world-api/hello-world-api.deployer.ts
@@ -1,14 +1,11 @@
 import { Ssh, SshOptions } from '@greeters/development.deployers.ssh-deployer';
 
-const lightsailOptions = {
-  host: '13.228.159.236',
-  username: 'bitnami',
-  cwd: './api',
-  privateKey: 'key',
-  port: 22,
+const config: SshOptions = {
+  host: 'xxxx',
+  username: 'xxx',
+  cwd: './',
+  privateKey: '/Users/<my_user>/key.pem',
   runCommand: 'npm install && npm run build --if-present && forever stopall && nohup forever api/index.js  &> /dev/null &',
-} as SshOptions;
+};
 
-const HelloWorldAPI = new Ssh(lightsailOptions);
-
-export default HelloWorldAPI
+Ssh.deploy(config);
